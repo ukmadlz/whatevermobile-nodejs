d81c353be41f20360e1806a6ef0489dd0f2acdb1
Mike Elsmore <mike.elsmore@uk.ibm.com>
2016-09-02 11:37:18 +0200

    First pass at writing the send functionality for an SMS

--- examples/sendsms.js
+++ examples/sendsms.js
@@ -5,3 +5,11 @@ var WhateverMobile = new whatevermobile({
   username: 'test',
   password: 'test'
 });
+
+WhateverMobile.message.sendSms({
+  to: 'you',
+  from: 'me',
+  body: 'HelloWorld'
+}, function(err, resp, body) {
+  console.log(body);
+});
